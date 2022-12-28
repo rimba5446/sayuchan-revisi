48f667e227ad9b3710bbab1346ea345d7ebb1ed2
RaykujanK13 <54540238+rimba5446@users.noreply.github.com>
2022-12-28 17:19:25 +0700

    Update Config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
 		prefix: "s!", 
-		botToken: "ODY1NDgzOTI5NjEwODc5MDI2.GxObDV.Vkvapb8V0o-vI_rV4ypo7_QwFmYLUptYXmwoe8", 
+		botToken: "", 
 		clientId: "865483929610879026", 
 		geniusApiToken: "G8gLxl5bV1un9OfdvU1rVogzQpmztV5rLa79x6C_PXPOPnXpcWZ9PThVaDgMCrb-", 
 		webplayer: "https://webplayersayu.glitch.me", // optional
